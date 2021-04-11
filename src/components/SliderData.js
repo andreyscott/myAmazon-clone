bd6718b0b70c46ac1ea882249b466f96b753e2a0
andreyscott <62455784+andreyscott@users.noreply.github.com>
2021-04-11 20:31:05 +0100

    fixing images order

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,16 +1,8 @@
 export const SliderData = [
   {
     image:
-      'https://m.media-amazon.com/images/G/01/AmazonExports/Projects/HelpPage/header-image._CB1567543150_.png'
+      'https://m.media-amazon.com/images/G/01/AmazonExports/Projects/HelpPage/header-image._CB1567543150_.png' 
   },
-    {
-      image:
-        "https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-    },
-    {
-      image:
-        'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg'
-    },
     {
       image:
         'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Computers_1x._CB432469755_.jpg'
@@ -18,5 +10,13 @@ export const SliderData = [
     {
       image:
         'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Beauty_v2_en_US_1x._CB429089975_.jpg'
-    }
+    },
+    {
+      image:
+        "https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2021/img/Events/MothersDay/Homepage/T3_2021_MothersDay_GW_HomepageHero_Desktop_Gifting_1x_1500x600._CB658614325_.jpg"
+    },
+    {
+      image:
+        'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg'
+    },
   ];
